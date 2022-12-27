931b09bb027a75266f9f0e449c3abfc4b31c6232
Jayden <yawzyn001@mymail.sim.edu.sg>
2022-12-28 05:47:32 +0800

    contact page

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -3,7 +3,7 @@ import Layout from "../../components/Layout";
 export default function ContactPage() {
   return (
     <div>
-      <Layout>Home Page</Layout>
+      <Layout>Contact Page</Layout>
     </div>
   );
 }
