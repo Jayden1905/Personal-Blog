3a8cbceac52cbcac50fc90ff22bbca43c89337a2
Jayden <yawzyn001@mymail.sim.edu.sg>
2022-12-28 21:16:14 +0800

    scroll behavior fix

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,8 +1,12 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import { motion } from "framer-motion";
 import { pageTransition } from "../animation/motion";
 
 export default function Layout({ children }: { children: ReactNode }) {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  });
+
   return (
     <motion.div
       className="max-w-3xl mx-auto"
