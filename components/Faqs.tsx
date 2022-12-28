15e98a876533bc8c276bf72053b8af1a67ce75f3
Jayden <yawzyn001@mymail.sim.edu.sg>
2022-12-29 05:23:59 +0800

    done about me section

---
+++ components/Faqs.tsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import { motion, AnimateSharedLayout, AnimatePresence } from "framer-motion";
+import { IoIosArrowUp, IoIosArrowDown } from "react-icons/io";
+
+export default function Faqs() {
+  return (
+    <AnimateSharedLayout>
+      <motion.ul layout initial={{ borderRadius: 25 }}>
+        <Item
+          title="What languages do you work with?"
+          content="I have experience working with a variety of programming languages, including Javascript, Typescript and Python. I am always looking to learn and work with new technologies, so this list is always evolving."
+        />
+        <Item
+          title="What types of projects have you completed?"
+          content="I have worked on a wide range of projects. I am always looking for new challenges and opportunities to learn and grow as a developer."
+        />
+        <Item
+          title="How can readers get in touch with you?"
+          content="You can contact me through the contact form on my blog or through my social media accounts. I am always open to hearing from readers and am happy to answer any questions you may have or consider collaborating on a project."
+        />
+      </motion.ul>
+    </AnimateSharedLayout>
+  );
+}
+
+function Item({ content, title }: { content: string; title: string }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleOpen = () => setIsOpen(!isOpen);
+
+  return (
+    <motion.li
+      layout
+      className="mb-4 last:mb-0 p-4 cursor-pointer border-2 dark:border-zinc-800 border-gray-300"
+      onClick={toggleOpen}
+      initial={{ borderRadius: 10 }}
+    >
+      <motion.div layout className="flex justify-between items-center">
+        <h1 className="text-lg font-light">{title}</h1>
+        {isOpen ? <IoIosArrowUp /> : <IoIosArrowDown />}
+      </motion.div>
+      <div>
+        <AnimatePresence>
+          {isOpen && <Content content={content} />}
+        </AnimatePresence>
+      </div>
+    </motion.li>
+  );
+}
+
+function Content({ content }: { content: string }) {
+  return (
+    <motion.div
+      className="mt-4 font-extralight text-sm tracking-wide leading-6"
+      layout
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+    >
+      {content}
+    </motion.div>
+  );
+}
--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,9 +1,127 @@
+import { GetStaticProps, InferGetStaticPropsType } from "next";
+import Image from "next/image";
+import Link from "next/link";
+import Faqs from "../../components/Faqs";
 import Layout from "../../components/Layout";
+import NotionService from "../../services/service";
 
-export default function AboutPage() {
+export const getStaticProps: GetStaticProps = async () => {
+  const notionService = new NotionService();
+
+  const posts = await notionService.getPublishedBlogPosts();
+
+  return {
+    props: {
+      posts,
+    },
+    revalidate: 30,
+  };
+};
+
+export default function AboutPage({
+  posts,
+}: InferGetStaticPropsType<typeof getStaticProps>): JSX.Element {
+  const numberOfPosts = posts?.length;
   return (
-    <div>
-      <Layout>About Page</Layout>
-    </div>
+    <Layout>
+      <div className="flex flex-col gap-10 mb-10">
+        <h1 className="text-4xl mt-10 text-center tracking-wide font-light">
+          Meet Oddinary
+        </h1>
+        <div className="w-full h-96 rounded-2xl overflow-hidden">
+          <Image
+            className="w-full h-full object-cover object-center block"
+            src={"/me.png"}
+            alt="img"
+            width={1000}
+            height={10}
+          />
+        </div>
+        <h1 className="text-2xl tracking-wide font-light">
+          Sharing my coding journey and the lessons I've learned along the way
+          and continuing to learn and grow as a developer.
+        </h1>
+        <p className="font-light text-justify text-lg">
+          Hi there! My name is Kyaw Za Yan Naing and I am a web developer with a
+          passion for creating and problem-solving. I have been programming for
+          3 years and have experience working with languages such as javascript,
+          typescript and python. I am excited to share my knowledge and
+          experiences with the community through this blog, and I hope to
+          inspire and empower others to pursue a career in tech. Thank you for
+          joining me on this journey!
+        </p>
+        <div className="flex md:flex-row my-10 flex-col md:gap-20 gap-10 justify-center items-center">
+          <div className="text-center">
+            <h1 className="text-7xl font-light mb-4">3</h1>
+            <p className="text-sm font-extralight dark:opacity-40">
+              Years into web-dev
+            </p>
+          </div>
+          <div className="text-center">
+            <h1 className="text-7xl font-light mb-4">{numberOfPosts}</h1>
+            <p className="text-sm font-extralight dark:opacity-40">
+              Current blog posts
+            </p>
+          </div>
+          <div className="text-center">
+            <h1 className="text-7xl font-light mb-4">26</h1>
+            <p className="text-sm font-extralight dark:opacity-40">
+              Github Projects
+            </p>
+          </div>
+        </div>
+        <h1 className="text-3xl font-light">Vision</h1>
+        <div className="grid gap-5 md:grid-cols-2 grid-cols-1">
+          <div>
+            <h1 className="font-light mb-2">Learning and Growing Together</h1>
+            <p className="text-xs dark:opacity-30 font-extralight leading-5 tracking-wide">
+              Creating a community where software developers of all levels can
+              come together to learn, share their experiences, and support one
+              another in their professional growth.
+            </p>
+          </div>
+          <div>
+            <h1 className="font-light mb-2">
+              Sharing My Journey with the World
+            </h1>
+            <p className="text-xs dark:opacity-30 font-extralight leading-5 tracking-wide">
+              Documenting and sharing the journey of learning and growing as a
+              developer, in the hopes of inspiring others to pursue their own
+              passions and goals.
+            </p>
+          </div>
+          <div>
+            <h1 className="font-light mb-2">
+              Small Steps Toward a Better Future
+            </h1>
+            <p className="text-xs dark:opacity-30 font-extralight leading-5 tracking-wide">
+              Contributing to the advancement of technology in small but
+              meaningful ways, and to inspire others to do the same.
+            </p>
+          </div>
+          <div>
+            <h1 className="font-light mb-2">
+              Building a Stronger Tech Community
+            </h1>
+            <p className="text-xs dark:opacity-30 font-extralight leading-5 tracking-wide">
+              Bring together the like-minded individuals who are passionate
+              about programming and technology, and to foster a sense of
+              collaboration and support within the community.
+            </p>
+          </div>
+        </div>
+        <h1 className="font-light text-3xl">FAQs</h1>
+        <Faqs />
+        <div className="font-light text-md text-black group hover:dark:text-white hover:text-black transition-all duration-300 ease-out">
+          <Link
+            scroll={false}
+            href={"/contact"}
+            className="dark:bg-zinc-200 bg-gray-300 hover:dark:bg-transparent hover:bg-transparent py-4 px-8 rounded-full border-2 hover:dark:border-zinc-800 hover:border-gray-300 transition-all duration-300 ease-out"
+          >
+            Get in touch
+          </Link>
+        </div>
+      </div>
+    </Layout>
   );
 }
