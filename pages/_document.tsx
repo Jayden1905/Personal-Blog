d98a2a8a3124ca0570d2522b268e91016c455f74
Jayden <yawzyn001@mymail.sim.edu.sg>
2023-01-01 18:21:06 +0800

    fix document structure

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,13 @@
 import NextDocument, { Html, Head, Main, NextScript } from "next/document";
 
-export default class Document extends NextDocument {
-  render() {
-    return (
-      <Html lang="en">
-        <Head />
-        <body className="dark:bg-zinc-900 bg-zinc-200 font-ibm !scroll-auto">
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body className="!scroll-auto bg-zinc-200 font-ibm dark:bg-zinc-900">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
