7f36d88a7cddee2f5966337f2472d8c587cff70e
Jayden <yawzyn001@mymail.sim.edu.sg>
2023-01-02 19:18:36 +0800

    add new favicons

--- components/layout/main.tsx
+++ components/layout/main.tsx
@@ -9,26 +9,43 @@ function RootLayout ({ children }: { children: ReactNode }) {
   return (
     <div>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Oddinary's Blog" />
-        <meta name="author" content="Kyaw Za Yan Naing" />
-        <meta name="author" content="Oddinary" />
-        <meta property="og:title" content="Oddinary's Blog" />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://oddinary.vercel.app/" />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta name='description' content="Oddinary's Blog" />
+        <meta name='author' content='Kyaw Za Yan Naing' />
+        <meta name='author' content='Oddinary' />
+        <meta property='og:title' content="Oddinary's Blog" />
+        <meta property='og:type' content='website' />
+        <meta property='og:url' content='https://oddinary.vercel.app/' />
         <meta
-          property="og:image"
-          content="https://oddinary.vercel.app/images/oddinary.png"
+          property='og:image'
+          content='https://oddinary.vercel.app/images/oddinary.png'
         />
-        <meta property="og:description" content="Oddinary's Blog" />
-        <meta property="og:site_name" content="Oddinary's Blog" />
+        <meta property='og:description' content="Oddinary's Blog" />
+        <meta property='og:site_name' content="Oddinary's Blog" />
 
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='/apple-touch-icon.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='/favicon-32x32.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='/favicon-16x16.png'
+        />
+        <link rel='manifest' href='/site.webmanifest' />
         <title>Oddinary&apos;s Blog</title>
       </Head>
 
       <ContextProvider>
-        <ThemeProvider enableSystem attribute="class">
+        <ThemeProvider enableSystem attribute='class'>
           <Nav />
           {children}
           <Footer />
