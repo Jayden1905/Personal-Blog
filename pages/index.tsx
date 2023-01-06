bee9263edf7ff1599990c3673e3ddded0ab25f58
Jayden <yawzyn001@mymail.sim.edu.sg>
2023-01-07 02:34:25 +0800

    change home page to ssr

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
-import type { GetStaticProps, InferGetStaticPropsType, NextPage } from 'next'
+import type {
+  GetServerSideProps,
+  InferGetServerSidePropsType,
+  NextPage
+} from 'next'
 import Head from 'next/head'
 import NotionService from '../services/service'
 import Link from 'next/link'
@@ -8,7 +12,12 @@ import BlogWrapper from '../components/BlogWrapper'
 import Image from 'next/image'
 import { NoScrollLayout } from '../components/layout/NoScrollLayout'
 
-export const getStaticProps: GetStaticProps = async () => {
+export const getServerSideProps: GetServerSideProps = async ({ res }) => {
+  res.setHeader(
+    'Cache-Control',
+    'public, s-maxage=10, stale-while-revalidate=30'
+  )
+
   const notionService = new NotionService()
 
   const posts = await notionService.getPublishedBlogPosts()
@@ -16,14 +25,13 @@ export const getStaticProps: GetStaticProps = async () => {
   return {
     props: {
       posts
-    },
-    revalidate: 30
+    }
   }
 }
 
 const Home: NextPage = ({
   posts
-}: InferGetStaticPropsType<typeof getStaticProps>) => {
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   const title = "Oddinary's Blog"
   const description = 'Welcome to my blog!'
 
@@ -41,50 +49,50 @@ const Home: NextPage = ({
         <Search posts={posts} />
         <BlogWrapper posts={posts} />
         <div>
-          <h1 className="mb-10 text-xl font-bold tracking-wider underline decoration-zinc-400 decoration-4 underline-offset-8 dark:decoration-zinc-700">
+          <h1 className='mb-10 text-xl font-bold tracking-wider underline decoration-zinc-400 decoration-4 underline-offset-8 dark:decoration-zinc-700'>
             Created With
           </h1>
-          <div className="mb-6 grid grid-cols-1 gap-10 sm:grid-cols-2 sm:gap-4">
-            <Link href={'https://nextjs.org/'} target="_blank">
-              <div className="flex flex-col items-center justify-center gap-2">
-                <div className="relative h-64 w-full sm:h-52">
+          <div className='mb-6 grid grid-cols-1 gap-10 sm:grid-cols-2 sm:gap-4'>
+            <Link href={'https://nextjs.org/'} target='_blank'>
+              <div className='flex flex-col items-center justify-center gap-2'>
+                <div className='relative h-64 w-full sm:h-52'>
                   <Image
-                    src="https://miro.medium.com/max/1400/1*2tmzU7bve-VlTkOMWsk_Hw.jpeg"
-                    alt="nextjs"
+                    src='https://miro.medium.com/max/1400/1*2tmzU7bve-VlTkOMWsk_Hw.jpeg'
+                    alt='nextjs'
                     fill
-                    className="h-full w-full rounded-2xl object-cover object-center"
-                    sizes="(max-width: 768px) 100vw,
+                    className='h-full w-full rounded-2xl object-cover object-center'
+                    sizes='(max-width: 768px) 100vw,
                       (max-width: 1200px) 50vw,
-                      33vw"
-                    placeholder="blur"
-                    blurDataURL="https://miro.medium.com/max/1400/1*2tmzU7bve-VlTkOMWsk_Hw.jpeg"
+                      33vw'
+                    placeholder='blur'
+                    blurDataURL='https://miro.medium.com/max/1400/1*2tmzU7bve-VlTkOMWsk_Hw.jpeg'
                     priority={true}
                   />
                 </div>
-                <h1 className="text-xl font-normal">Next Js</h1>
-                <p className="text-md font-extralight">
+                <h1 className='text-xl font-normal'>Next Js</h1>
+                <p className='text-md font-extralight'>
                   Frame Work for building full-stack websites
                 </p>
               </div>
             </Link>
-            <Link href={'https://www.notion.so/'} target="_blank">
-              <div className="flex flex-col items-center justify-center gap-2">
-                <div className="relative h-64 w-full rounded-2xl bg-gray-200 sm:h-52">
+            <Link href={'https://www.notion.so/'} target='_blank'>
+              <div className='flex flex-col items-center justify-center gap-2'>
+                <div className='relative h-64 w-full rounded-2xl bg-gray-200 sm:h-52'>
                   <Image
-                    src="https://itbabble.files.wordpress.com/2021/11/notion.png"
-                    alt="notion"
-                    className="h-full w-full rounded-2xl object-cover object-center"
+                    src='https://itbabble.files.wordpress.com/2021/11/notion.png'
+                    alt='notion'
+                    className='h-full w-full rounded-2xl object-cover object-center'
                     fill
-                    sizes="(max-width: 768px) 100vw,
+                    sizes='(max-width: 768px) 100vw,
                       (max-width: 1200px) 50vw,
-                      33vw"
-                    placeholder="blur"
-                    blurDataURL="https://miro.medium.com/max/1000/1*htbUdWgFQ3a94PMEvBr_hQ.png"
+                      33vw'
+                    placeholder='blur'
+                    blurDataURL='https://miro.medium.com/max/1000/1*htbUdWgFQ3a94PMEvBr_hQ.png'
                     priority={true}
                   />
                 </div>
-                <h1 className="text-xl font-normal">Notion</h1>
-                <p className="text-md font-extralight">
+                <h1 className='text-xl font-normal'>Notion</h1>
+                <p className='text-md font-extralight'>
                   A note-taking application for CMS
                 </p>
               </div>
