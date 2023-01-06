2333a34bb5f66b7c10b6cfd9f9ea898c31d3c3e5
Jayden <yawzyn001@mymail.sim.edu.sg>
2023-01-07 05:47:03 +0800

    trying normal function

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,15 +1,17 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  await res.revalidate('/')
+const handler = (req: NextApiRequest, res: NextApiResponse) => {
+  res.revalidate('/')
 
   const pathToRevalidate = `/posts/${
     req.body?.record?.slug || req.body?.old_record?.slug
   }`
 
-  const categoriesPathToRevalidate = '/categories/'
+  const categoriesPath = `/categories/${
+    req.body?.record?.category || req.body?.old_record?.category
+  }`
 
-  res.revalidate(categoriesPathToRevalidate)
+  res.revalidate(categoriesPath)
 
   res.revalidate(pathToRevalidate)
 
