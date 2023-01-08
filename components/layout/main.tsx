e0da669cca866a9574f0f1dbe814ebbc3155dc8f
Jayden <yawzyn001@mymail.sim.edu.sg>
2023-01-08 21:11:35 +0800

    add partytown

--- components/layout/main.tsx
+++ components/layout/main.tsx
@@ -4,47 +4,49 @@ import { ThemeProvider } from 'next-themes'
 import Nav from '../Nav'
 import Footer from '../Footer'
 import ContextProvider from '../context/ContextProvider'
+import { Partytown } from '@builder.io/partytown/react'
 
 function RootLayout ({ children }: { children: ReactNode }) {
   return (
     <div>
       <Head>
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <meta name='description' content="Oddinary's Blog" />
-        <meta name='author' content='Kyaw Za Yan Naing' />
-        <meta name='author' content='Oddinary' />
-        <meta property='og:title' content="Oddinary's Blog" />
-        <meta property='og:type' content='website' />
-        <meta property='og:url' content='https://oddinary.vercel.app/' />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Oddinary's Blog" />
+        <meta name="author" content="Kyaw Za Yan Naing" />
+        <meta name="author" content="Oddinary" />
+        <meta property="og:title" content="Oddinary's Blog" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://oddinary.vercel.app/" />
         <meta
-          property='og:image'
-          content='https://oddinary.vercel.app/images/oddinary.png'
+          property="og:image"
+          content="https://oddinary.vercel.app/images/oddinary.png"
         />
-        <meta property='og:description' content="Oddinary's Blog" />
-        <meta property='og:site_name' content="Oddinary's Blog" />
+        <meta property="og:description" content="Oddinary's Blog" />
+        <meta property="og:site_name" content="Oddinary's Blog" />
 
         <link
-          rel='apple-touch-icon'
-          sizes='180x180'
-          href='/apple-touch-icon.png'
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
         />
         <link
-          rel='icon'
-          type='image/png'
-          sizes='32x32'
-          href='/favicon-32x32.png'
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
         />
         <link
-          rel='icon'
-          type='image/png'
-          sizes='16x16'
-          href='/favicon-16x16.png'
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
         />
         <title>Oddinary&apos;s Blog</title>
+        <Partytown debug={true} forward={['dataLayer.push']} />
       </Head>
 
       <ContextProvider>
-        <ThemeProvider enableSystem attribute='class'>
+        <ThemeProvider enableSystem attribute="class">
           <Nav />
           {children}
           <Footer />
