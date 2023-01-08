3c8e770736e12d91a5277cf67b94d339419f3e39
Jayden <yawzyn001@mymail.sim.edu.sg>
2023-01-08 17:15:44 +0800

    remove posts/path

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -4,11 +4,6 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     await res.revalidate('/')
 
-    const postsPath = `/posts/${
-      req.body?.record?.slug || req.body?.old_record?.slug
-    }`
-    await res.revalidate(postsPath)
-
     const categoriesPath = `/categories/${
       req.body?.record?.tag || req.body?.old_record?.tag
     }`
